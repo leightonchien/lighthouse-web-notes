933f3ceb7945b6924fc060433734e6f9abdd8a21
leightonchien <leightonchien@gmail.com>
2021-03-08 05:27:56 +0000

    Using array .filter function to print out only passing grades

---
+++ w1/d4-funcs/filtering/passingGradesOnly.js
@@ -0,0 +1,7 @@
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+
+const passingGrade = grades.filter(grade => grade >= 70);
+
+console.log(passingGrade);
+
+//result [ 73, 100, 70, 88, 95, 77 ]
