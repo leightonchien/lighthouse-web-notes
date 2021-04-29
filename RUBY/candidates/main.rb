5d267e54922e5d04d9aa61c50691ee5c890f7fba
Leighton Chien <leightonchien@gmail.com>
2021-04-29 00:44:53 -0700

    qualified_candidates method added

--- RUBY/candidates/main.rb
+++ RUBY/candidates/main.rb
@@ -1,5 +1,6 @@
 # This is the main entrypoint into the program
 # It requires the other files/gems that it needs
+
 require 'pp'
 require './candidates'
 require './filters'
@@ -7,15 +8,15 @@ require './filters'
 ## Your test code can go here
 
 def qualified_candidates(candidates)
-    puts "Candidates that meet your specs:"
-  
-    qualified = candidates.select do |c|
-      experienced?(c)
-      has_enough_github_pts?(c) &&
-      has_right_languages?(c) &&
-      applied_within_15_days?(c) &&
-      is_of_age?(c)
-    end
+  puts "Candidates that meet your specs:"
+
+  qualified = candidates.select do |c|
+    experienced?(c)
+    has_enough_github_pts?(c) &&
+    has_right_languages?(c) &&
+    applied_within_15_days?(c) &&
+    is_of_age?(c)
   end
-  
-  pp qualified_candidates(@candidates)
+end
+
+pp qualified_candidates(@candidates)
