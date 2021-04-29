8d4bb87d84cb88f8dc32eef1d7bd7534fff9a2e9
Leighton Chien <leightonchien@gmail.com>
2021-04-29 00:40:26 -0700

    Methods 'experiences?' and 'find' completed

--- RUBY/candidates/filters.rb
+++ RUBY/candidates/filters.rb
@@ -3,10 +3,14 @@
 
 def find(id)
     # Your code Here
+    @candidates.detect {|x| x[:id] == id ? x : nil}
   end
   
   def experienced?(candidate)
     # Your code Here
+    yes_to_exp = candidate[:years_of_experience] >= 2
+    return "Candidate has the requested years of experience: #{yes_to_exp}"
+
   end
   
   def qualified_candidates(candidates)
@@ -14,3 +18,44 @@ def find(id)
   end
   
   # More methods will go below
+
+  # def find(id)
+#   @candidates.detect do |x|
+#     if x[:id] == id
+#       return x
+#     else
+#       nil
+#     end
+#   end
+# end
+
+# def find(id)
+#   @candidates.each do |ele|
+#     if ele[:id] == id
+#       return @candidate[ele]
+#     else
+#       nil
+#     end
+#   end
+# end
+
+# def find(id) #takes in id
+#   #returns candidate with that id
+#   #if there is no candidate with that id, it returns nil
+#   @candidates.each do |ele|
+#     if ele[:id] == id
+#       return @candidate[ele]
+#     else
+#       nil
+#   end
+# end
+
+# def experienced?(candidate)
+#   # this takes in a single 'candidate' hash (not array)
+#   # Returns true if candidate has 2 or more of experience
+#   # Returns false otherwise
+
+#   yes_to_exp = candidate[:years_of_experience] >= 2
+#   return "Candidate has the requested years of experience: #{yes_to_exp}"
+
+# end
--- RUBY/candidates/main.rb
+++ RUBY/candidates/main.rb
@@ -6,4 +6,16 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+def qualified_candidates(candidates)
+    puts "Candidates that meet your specs:"
+  
+    qualified = candidates.select do |c|
+      experienced?(c)
+      has_enough_github_pts?(c) &&
+      has_right_languages?(c) &&
+      applied_within_15_days?(c) &&
+      is_of_age?(c)
+    end
+  end
+  
+  pp qualified_candidates(@candidates)
